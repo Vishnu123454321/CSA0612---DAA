e46f86461a1ff7d5f10443fb4325fe3674fa66ac
Vishnu123454321 <132432209+Vishnu123454321@users.noreply.github.com>
2023-11-22 09:06:49 +0530

    MST using greedy techniques

--- DAA 20 th program.cpp
+++ DAA 20 th program.cpp
@@ -1,4 +1,3 @@
-
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -6,7 +5,6 @@
 int minKey(int key[], bool mstSet[])
 {
 	int min = INT_MAX, min_index;
-
 	for (int v = 0; v < V; v++)
 		if (mstSet[v] == false && key[v] < min)
 			min = key[v], min_index = v;
